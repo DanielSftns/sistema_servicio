918544fc39327e6f084feda5c47f11ba263527bc
daniel <danielsifontes99@gmail.com>
2022-03-03 00:03:06 -0400

    empezando a usar chakra UI

--- src/App.js
+++ src/App.js
@@ -10,18 +10,27 @@ import Login from './components/Login';
 import Register from './components/Register';
 import PanelEstudiante from './components/estudiante/PanelEstudiante';
 
+import {
+  Box
+} from '@chakra-ui/react'
+
+
 function App() {
   return (
-    <div className="container">
+    <Box
+      maxW={1024}
+      m="auto"
+    >
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/registro' element={<Register />} />
           <Route path='/estudiante' element={<PanelEstudiante />} />
+          <Route path='/' element={<h1>landing</h1>} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </Box>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  Flex,
+  Box
+} from '@chakra-ui/react'
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/estudiante">SERVICIO</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" to="/login">Iniciar Sesion</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="registro">Registrarse</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Flex
+      py={2}
+      justifyContent='space-between'
+      alignItems='center'
+    >
+      <Box fontWeight="700" fontSize="2xl">
+        <Link to="/">SERVICIO</Link>
+      </Box>
+      <Flex gap={2} >
+        <Box>
+          <Link to="/login">Iniciar Sesion</Link>
+        </Box>
+        <Box>
+          <Link to="registro">Registrarse</Link>
+        </Box>
+      </Flex>
+    </Flex>
   );
 }
  

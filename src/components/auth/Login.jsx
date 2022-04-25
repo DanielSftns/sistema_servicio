f6a37586977edde1fbbc9965429ebdfb33198e6a
Daniel Sifontes <danielsifontes99@gmail.com>
2022-04-25 18:19:25 -0400

    bug fix login

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
           break
         }
         case 'tutor etapa cumplimiento': {
-          nextPage = 'tutor/proyectos'
+          nextPage = '/tutor/proyectos'
           break
         }
 
